4214ceec6184e00664ce95ce54110d7887bee537
Nibofar <quentin.bouletbenac@gmail.com>
2022-10-06 15:23:10 +0200

    update2

--- Cours2/IntArray.hpp
+++ Cours2/IntArray.hpp
@@ -93,3 +93,16 @@ protected:
     int* data = nullptr;
     int size = 0;
 };
+void testArray() {
+    IntArray muttsu(5);
+    muttsu.set(0, 8);
+    muttsu.set(1, 2);
+    muttsu.set(2, 13);
+    muttsu.set(3, 9);
+    muttsu.set(4, 1);
+    muttsu.sort();
+    /*muttsu.insertOrderInferior(21); // order sur A par le prédicat f : a[i] < a[i+1]
+    muttsu.insertOrderInferior(-4);
+    muttsu.insertOrderInferior(54);*/
+    assert(muttsu.isSorted());
+}
